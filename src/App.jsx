1c995aa9a5d0628ae83591a1b422c6569b501d62
Waqar_Abbas <waqarabas72@gmail.com>
2024-04-16 22:44:37 +0500

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Content from "./components/Content/Content";
 import Header from "./components/Header/Header";
+import Skills from "./components/Skills";
 
 function App() {
   return (
     <>
       <Header />
       <Content />
+      <Skills />
     </>
   );
 }
