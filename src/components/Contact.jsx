8e5296287c64af0fd9374fa3345054fd50a6e0b8
Waqar_Abbas <waqarabas72@gmail.com>
2024-04-24 00:32:34 +0500

    contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,191 +1,176 @@
-import { useState } from "react";
-import { Switch } from "@headlessui/react";
+import React, { useState } from "react";
+import emailjs from "emailjs-com";
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
-}
+function Contact() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
 
-export default function Contact() {
-  const [agreed, setAgreed] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setIsLoading(true);
+    setIsSuccess(false);
+    setIsError(false);
+
+    try {
+      const response = await emailjs.send(
+        "service_li2ogeh", // Replace with your EmailJS service ID
+        "template_5ukv72p", // Replace with your EmailJS template ID
+        formData,
+        "kWbXdAd-x1po0LkwH" // Replace with your EmailJS user ID
+      );
+      if (response.status === 200) {
+        setFormData({ name: "", email: "", message: "" });
+        setIsLoading(false);
+        setIsSuccess(true);
+      } else {
+        setIsLoading(false);
+        setIsError(true);
+      }
+    } catch (error) {
+      console.error("Email sending error:", error);
+      setIsLoading(false);
+      setIsError(true);
+    }
+  };
 
   return (
-    <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
-      <div
-        className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
-        aria-hidden="true"
+    <form
+      onSubmit={handleSubmit}
+      className="flex justify-center items-center flex-col"
+    >
+      {/* Form fields to capture user input */}
+      <input
+        type="text"
+        name="name"
+        value={formData.name}
+        onChange={handleChange}
+        placeholder="Your Name"
+        required
+        className="border"
+      />
+      <input
+        type="email"
+        name="email"
+        value={formData.email}
+        onChange={handleChange}
+        placeholder="Your Email"
+        required
+        className="border"
+      />
+      <textarea
+        name="message"
+        value={formData.message}
+        onChange={handleChange}
+        placeholder="Your Message"
+        required
+        className="border"
+      />
+      <button
+        type="submit"
+        disabled={isLoading}
+        className="text-white py-2 px-4 mr-3 font-bold bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 my-3"
       >
-        <div
-          className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
-          style={{
-            clipPath:
-              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-          }}
-        />
-      </div>
-      <div className="mx-auto max-w-2xl text-center">
-        <p className="font-bold text-4xl text-center text-gray-800 tracking-widest">
-          CONTACT ME !
-        </p>
-        <p className="mt-2 text-lg leading-8 text-gray-600 font-bold">
-          Lets Bring Your Ideas Into Life!
-        </p>
-      </div>
-      <form action="" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
-        <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-          <div>
-            <label
-              htmlFor="first-name"
-              className="block text-sm font-semibold leading-6 text-gray-900"
-            >
-              First name
-            </label>
-            <div className="mt-2.5">
-              <input
-                type="text"
-                name="first-name"
-                id="first-name"
-                autoComplete="given-name"
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-          <div>
-            <label
-              htmlFor="last-name"
-              className="block text-sm font-semibold leading-6 text-gray-900"
-            >
-              Last name
-            </label>
-            <div className="mt-2.5">
-              <input
-                type="text"
-                name="last-name"
-                id="last-name"
-                autoComplete="family-name"
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-          <div className="sm:col-span-2">
-            <label
-              htmlFor="company"
-              className="block text-sm font-semibold leading-6 text-gray-900"
-            >
-              Company
-            </label>
-            <div className="mt-2.5">
-              <input
-                type="text"
-                name="company"
-                id="company"
-                autoComplete="organization"
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-          <div className="sm:col-span-2">
-            <label
-              htmlFor="email"
-              className="block text-sm font-semibold leading-6 text-gray-900"
-            >
-              Email
-            </label>
-            <div className="mt-2.5">
-              <input
-                type="email"
-                name="email"
-                id="email"
-                autoComplete="email"
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-          <div className="sm:col-span-2">
-            <label
-              htmlFor="phone-number"
-              className="block text-sm font-semibold leading-6 text-gray-900"
-            >
-              Phone number
-            </label>
-            <div className="relative mt-2.5">
-              <div className="absolute inset-y-0 left-0 flex items-center">
-                <label htmlFor="country" className="sr-only">
-                  Country
-                </label>
-                <select
-                  id="country"
-                  name="country"
-                  className="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-9 text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
-                >
-                  <option>US</option>
-                  <option>CA</option>
-                  <option>EU</option>
-                </select>
-              </div>
-              <input
-                type="number"
-                name="phone-number"
-                id="phone-number"
-                autoComplete="tel"
-                className="block w-full rounded-md border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-          <div className="sm:col-span-2">
-            <label
-              htmlFor="message"
-              className="block text-sm font-semibold leading-6 text-gray-900"
-            >
-              Message
-            </label>
-            <div className="mt-2.5">
-              <textarea
-                name="message"
-                id="message"
-                rows={4}
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                defaultValue={""}
-              />
-            </div>
-          </div>
-          <Switch.Group as="div" className="flex gap-x-4 sm:col-span-2">
-            <div className="flex h-6 items-center">
-              <Switch
-                checked={agreed}
-                onChange={setAgreed}
-                className={classNames(
-                  agreed ? "bg-indigo-600" : "bg-gray-200",
-                  "flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                )}
-              >
-                <span className="sr-only">Agree to policies</span>
-                <span
-                  aria-hidden="true"
-                  className={classNames(
-                    agreed ? "translate-x-3.5" : "translate-x-0",
-                    "h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out"
-                  )}
-                />
-              </Switch>
-            </div>
-            <Switch.Label className="text-sm leading-6 text-gray-600">
-              By selecting this, you agree to our{" "}
-              <a href="#" className="font-semibold text-indigo-600">
-                privacy&nbsp;policy
-              </a>
-              .
-            </Switch.Label>
-          </Switch.Group>
-        </div>
-        <div className="mt-10">
-          <button
-            type="submit"
-            className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            {`Let's Talk`}
-          </button>
-        </div>
-      </form>
-    </div>
+        {isLoading ? "Sending..." : "Send Email"}
+      </button>
+      {isSuccess && <p className="success">Email sent successfully!</p>}
+      {isError && (
+        <p className="error">Error sending email. Please try again.</p>
+      )}
+    </form>
+    // <form onSubmit={handleSubmit}>
+    //   <div className="space-y-12">
+    //     <div className="border-b border-gray-900/10 pb-12">
+    //       <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+    //         <div className="sm:col-span-4">
+    //           <label
+    //             htmlFor="username"
+    //             className="block text-sm font-medium leading-6 text-gray-900"
+    //           >
+    //             Name
+    //           </label>
+    //           <div className="mt-2">
+    //             <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+    //               <input
+    //                 type="text"
+    //                 name="name"
+    //                 value={formData.name}
+    //                 onChange={handleChange}
+    //                 placeholder="Your Name"
+    //                 required
+    //                 className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+    //               />
+    //             </div>
+    //           </div>
+    //         </div>
+
+    //         <div className="sm:col-span-4">
+    //           <label
+    //             htmlFor="username"
+    //             className="block text-sm font-medium leading-6 text-gray-900"
+    //           >
+    //             Email
+    //           </label>
+    //           <div className="mt-2">
+    //             <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+    //               <input
+    //                 type="email"
+    //                 name="email"
+    //                 value={formData.email}
+    //                 onChange={handleChange}
+    //                 placeholder="Your Email"
+    //                 required
+    //                 className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+    //               />
+    //             </div>
+    //           </div>
+    //         </div>
+
+    //         <div className="col-span-full">
+    //           <label
+    //             htmlFor="about"
+    //             className="block text-sm font-medium leading-6 text-gray-900"
+    //           >
+    //             Message
+    //           </label>
+    //           <div className="mt-2">
+    //             <textarea
+    //               name="message"
+    //               value={formData.message}
+    //               onChange={handleChange}
+    //               placeholder="Your Message"
+    //               required
+    //               className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+    //               defaultValue={""}
+    //             />
+    //           </div>
+    //           <button
+    //             type="submit"
+    //             disabled={isLoading}
+    //             className="text-white py-2 px-4 mr-3 font-bold bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 my-3"
+    //           >
+    //             {isLoading ? "Sending..." : "Send Email"}
+    //           </button>
+    //           {isSuccess && <p className="success">Email sent successfully!</p>}
+    //           {isError && (
+    //             <p className="error">Error sending email. Please try again.</p>
+    //           )}
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </form>
   );
 }
+
+export default Contact;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -65,11 +65,11 @@ export default function Projects() {
         <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
           {products.map((product) => (
             <div key={product.id} className="group">
-              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7 shadow-xl">
+              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg xl:aspect-h-8 xl:aspect-w-7 shadow-xl">
                 <img
                   src={product.imageSrc}
                   alt={product.imageAlt}
-                  className="h-72 w-72 object-cover object-center group-hover:opacity-75"
+                  className="h-72 w-auto sm:w-72 object-cover object-center group-hover:opacity-75"
                 />
               </div>
               <h3 className="mt-4 font-bold text-gray-800">{product.name}</h3>
