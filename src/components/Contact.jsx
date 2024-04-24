28423cd13d007fe8e14660be762212c37f844743
Waqar_Abbas <waqarabas72@gmail.com>
2024-04-24 17:13:53 +0500

    change

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -45,131 +45,81 @@ function Contact() {
   };
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      className="flex justify-center items-center flex-col"
-    >
-      {/* Form fields to capture user input */}
-      <input
-        type="text"
-        name="name"
-        value={formData.name}
-        onChange={handleChange}
-        placeholder="Your Name"
-        required
-        className="border"
-      />
-      <input
-        type="email"
-        name="email"
-        value={formData.email}
-        onChange={handleChange}
-        placeholder="Your Email"
-        required
-        className="border"
-      />
-      <textarea
-        name="message"
-        value={formData.message}
-        onChange={handleChange}
-        placeholder="Your Message"
-        required
-        className="border"
-      />
-      <button
-        type="submit"
-        disabled={isLoading}
-        className="text-white py-2 px-4 mr-3 font-bold bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 my-3"
+    <>
+      <p className="font-bold text-4xl text-center text-gray-800 tracking-widest my-4">
+        CONTACT ME NOW!
+      </p>
+      <form
+        onSubmit={handleSubmit}
+        className="flex justify-center items-center flex-col"
       >
-        {isLoading ? "Sending..." : "Send Email"}
-      </button>
-      {isSuccess && <p className="success">Email sent successfully!</p>}
-      {isError && (
-        <p className="error">Error sending email. Please try again.</p>
-      )}
-    </form>
-    // <form onSubmit={handleSubmit}>
-    //   <div className="space-y-12">
-    //     <div className="border-b border-gray-900/10 pb-12">
-    //       <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-    //         <div className="sm:col-span-4">
-    //           <label
-    //             htmlFor="username"
-    //             className="block text-sm font-medium leading-6 text-gray-900"
-    //           >
-    //             Name
-    //           </label>
-    //           <div className="mt-2">
-    //             <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-    //               <input
-    //                 type="text"
-    //                 name="name"
-    //                 value={formData.name}
-    //                 onChange={handleChange}
-    //                 placeholder="Your Name"
-    //                 required
-    //                 className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-    //               />
-    //             </div>
-    //           </div>
-    //         </div>
+        {/* Form fields to capture user input */}
+        <div>
+          <label
+            htmlFor="price"
+            className="block text-sm font-medium leading-6 text-gray-700"
+          >
+            Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            placeholder="Your Name"
+            required
+            className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+        </div>
 
-    //         <div className="sm:col-span-4">
-    //           <label
-    //             htmlFor="username"
-    //             className="block text-sm font-medium leading-6 text-gray-900"
-    //           >
-    //             Email
-    //           </label>
-    //           <div className="mt-2">
-    //             <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-    //               <input
-    //                 type="email"
-    //                 name="email"
-    //                 value={formData.email}
-    //                 onChange={handleChange}
-    //                 placeholder="Your Email"
-    //                 required
-    //                 className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-    //               />
-    //             </div>
-    //           </div>
-    //         </div>
+        <div className="my-4">
+          <label
+            htmlFor="price"
+            className="block text-sm font-medium leading-6 text-gray-700"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            placeholder="Your Email"
+            required
+            className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+        </div>
 
-    //         <div className="col-span-full">
-    //           <label
-    //             htmlFor="about"
-    //             className="block text-sm font-medium leading-6 text-gray-900"
-    //           >
-    //             Message
-    //           </label>
-    //           <div className="mt-2">
-    //             <textarea
-    //               name="message"
-    //               value={formData.message}
-    //               onChange={handleChange}
-    //               placeholder="Your Message"
-    //               required
-    //               className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-    //               defaultValue={""}
-    //             />
-    //           </div>
-    //           <button
-    //             type="submit"
-    //             disabled={isLoading}
-    //             className="text-white py-2 px-4 mr-3 font-bold bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 my-3"
-    //           >
-    //             {isLoading ? "Sending..." : "Send Email"}
-    //           </button>
-    //           {isSuccess && <p className="success">Email sent successfully!</p>}
-    //           {isError && (
-    //             <p className="error">Error sending email. Please try again.</p>
-    //           )}
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </form>
+        <div>
+          <label
+            htmlFor="price"
+            className="block text-sm font-medium leading-6 text-gray-700"
+          >
+            Your Message
+          </label>
+          <textarea
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+            placeholder="Your Message"
+            cols={24}
+            required
+            className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+        </div>
+        <button
+          type="submit"
+          disabled={isLoading}
+          className="py-2 px-4 mr-3 font-bold my-4 border text-gray-700 cursor-pointer hover:border-gray-700 duration-300 rounded-lg"
+        >
+          {isLoading ? "Sending..." : "Send Message"}
+        </button>
+        {isSuccess && <p className="success">Email sent successfully!</p>}
+        {isError && (
+          <p className="error">Error sending email. Please try again.</p>
+        )}
+      </form>
+    </>
   );
 }
 
