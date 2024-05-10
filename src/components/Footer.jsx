eb5829572cbe690dda1372c6753e10861ea12ac1
Waqar_Abbas <waqarabas72@gmail.com>
2024-05-10 22:03:50 +0500

    change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='bg-gray-900 text-gray-300 text-center p-3 tracking-wider font-bold'>
-        Copyright &copy; 2024.All rights reserved
+       <p>Reach Me Out</p>
+        <SocialMedia
+            className={`flex items-center py-4 gap-4 justify-center`}
+          />
     </div>
   )
 }
