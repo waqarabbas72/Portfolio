49e2767329d387aeaa600c80eddb0d53d6c744f3
Waqar_Abbas <waqarabas72@gmail.com>
2024-05-19 19:23:14 +0500

    change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import { FaArrowAltCircleRight } from 'react-icons/fa'
 const Footer = () => {
   return (
     <div className='bg-gray-100 text-center mt-10 tracking-wider font-bold flex items-center justify-around'>
-       <p className='flex items-center gap-5'>REACH ME OUT <FaArrowAltCircleRight className='w-6 h-6' /></p>
+       <p className='flex items-center gap-5'>REACH ME OUT <FaArrowAltCircleRight className='w-6 h-6 hidden sm:block' /></p>
         <SocialMedia
             className={`flex items-center py-4 gap-4 justify-center`}
           />
